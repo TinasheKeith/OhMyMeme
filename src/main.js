8f6ff4f0605e5e0c1a127d8dfe034e258be248b3
tintin.dev <tinashe.madzimbamuto@younglings.africa>
2020-05-26 18:09:41 +0200

    WIP

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import VModal from "vue-js-modal";
+
+Vue.config.productionTip = false;
+
+Vue.use(VModal);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
